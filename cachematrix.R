0ee76a08206d3972d0c04809352704febc0725a2
anonypbc <77603649+anonypbc@users.noreply.github.com>
2023-07-29 17:00:14 +0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## I simply set the input x as a matrix
 ## and then set the solved value "s" as a null
-## then I changed every reference to "mean" to "solve"
+## then I changed every reference to "mean" to "solve" 
 makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
   s <- NULL
   set <- function(y) {
